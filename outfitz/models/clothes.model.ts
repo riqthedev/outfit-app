793c01d48004edd2a0ab6199fc74f813d9c4b0b2
riqthedev <riqthedev@gmail.com>
2024-07-07 02:56:23 -0400

    Created clothes and inspirtation models

---
+++ outfitz/models/clothes.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema, Document } from 'mongoose'
+
+interface Clothes {
+    user_id: number;
+    image_url: string;
+    item_type: string;
+    color: string;
+    pattern: string;
+    brand: string;
+    purchase_date: Date
+}
+
+const clothesSchema = new Schema({
+    user_id: {type: Number, required: true, ref:"User"},
+    brand: {type: String, required: true},
+    color: {type: String, required: true},
+    pattern: {type: String},
+    purchase_date: {type: Date, required: true},
+    item_type: {type: String, required: true},
+    image_url: {type: String, required: true},
+    
+})
+
+const Clothes = model <Clothes & Document>('Clothes', clothesSchema)
+
+export default Clothes;
---
+++ outfitz/models/inspiration.model.ts
@@ -0,0 +1,22 @@
+import { model, Schema, Document } from 'mongoose'
+
+
+interface Inspiration {
+    user_id: number,
+    image_url: string,
+    description: string
+}
+
+
+
+const inspirationSchema = new Schema ({
+    user_id: {type: Number, required: true, ref:"User"},
+    image_url: {type: String, required: true},
+    description: {type: String, required: true}
+
+})
+
+
+const Inspiration = model <Inspiration & Document>('Inspiration', inspirationSchema)
+
+export default Inspiration;
